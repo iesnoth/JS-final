2870fda0f80e34c7b7d7c417c91a36bc74250a83
Victoria Beasley <iesnoth91@gmail.com>
2022-09-19 18:29:51 -0500

    made a dropdown menu

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //make map                                            
-const map = L.map('map').setView([0,0], 3);
+const map = L.map('map').setView([0,0], 5);
 
 // add openstreetmap tiles
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -11,17 +11,11 @@ L.control.locate().addTo(map);
 setView(center,zoom)
 // Allow the user to select a business type from a list and
 //map the five nearest locations on the map using the Foursquare API.
+
+
 // What events will your application need?
 // What APIs will you need and in what order?
-// How will you obtain the user's location?
-// How will you add the user's location to the map?
-// How will you get the selected location from the user?
+
 // How will you add that information to the map?
 // Add a simple select interface for the user with the following
 // values: coffee, restaurant, hotel, and market.
-// Next, create a space where you will place your map.
-
-
-//PSEUDOCODE
-/*Set up the map based on the user's location.
- */
